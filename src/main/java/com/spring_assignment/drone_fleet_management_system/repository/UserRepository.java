fd1727ddfeeae1af1e45efa8b2c83c3079185bbd
Maksudur <progmaksudur@gmail.cm>
2024-10-07 19:38:40 +0600

    Some service create

--- src/main/java/com/spring_assignment/drone_fleet_management_system/repository/UserRepository.java
+++ src/main/java/com/spring_assignment/drone_fleet_management_system/repository/UserRepository.java
@@ -4,6 +4,14 @@ import com.spring_assignment.drone_fleet_management_system.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmailOrPhone(String email, String phone);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
+    Boolean existsByEmail(String email);
+    Boolean existsByPhone(String phone);
 }
--- src/main/java/com/spring_assignment/drone_fleet_management_system/service/UserService.java
+++ src/main/java/com/spring_assignment/drone_fleet_management_system/service/UserService.java
@@ -1,11 +1,30 @@
 package com.spring_assignment.drone_fleet_management_system.service;
 
 import com.spring_assignment.drone_fleet_management_system.model.request.UserSignUpRequest;
+import com.spring_assignment.drone_fleet_management_system.model.response.UserDTO;
+
+import java.util.List;
 
 public interface UserService {
 
 
     public void createUser(UserSignUpRequest userSignUpRequest);
+
+    public UserDTO getUserDetails(Long userID);
+
+    public UserDTO getUserDetailsByEmailOrPhone(String emailOrPhone);
+
+    public List<UserDTO> getAllUser();
+
+    public void updateUser();
+
+    public void deleteUser();
+
+    public void verifyUserByEmail();
+
+    public void verifyUserByPhone();
+
+
 
 
 
