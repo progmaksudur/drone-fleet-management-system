676ac41343ac1b8509a552f660f0ed76a7930752
Maksudur <progmaksudur@gmail.cm>
2024-10-07 19:38:03 +0600

    JWT Security Enable

---
+++ src/main/java/com/spring_assignment/drone_fleet_management_system/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.spring_assignment.drone_fleet_management_system.service;
+
+import com.spring_assignment.drone_fleet_management_system.entity.User;
+import com.spring_assignment.drone_fleet_management_system.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = (username.contains("@")?repository.findByEmail(username):repository.findByPhone(username))
+                .orElseThrow(()-> new UsernameNotFoundException("User not found with username: " + username));
+
+
+        return  org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[0]))
+                .build();
+    }
+}
